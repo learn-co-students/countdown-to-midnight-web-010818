0c675ab8d070b0aa1447c8e0bbb24c4578384dd1
Folu Obatusin <nisutabo@gmail.com>
2017-12-25 02:47:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    puts n.to_s + " SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  countdown(n)
+  sleep(5)
 end
